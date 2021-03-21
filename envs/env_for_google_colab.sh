387d685e003c9604a63f3b95b3e22437355e14cf
bitotest <bitotest@bito.ltd>
2021-03-21 23:52:44 +0800

    update envs files

--- envs/env_for_google_colab.sh
+++ envs/env_for_google_colab.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-git clone https://github.com/NVIDIA/apex.git apex && cd apex && python3 setup.py install --cuda_ext --cpp_ext
+git clone https://github.com/NVIDIA/apex.git apex && cd apex && python3 setup.py install
 
 pip install -q torch_snippets
-pip install -q pytorch_model_summary
-pip install -q pyyaml>=5.1.1
-pip install -q coolname>=1.1.0
-pip install -q tabulate>=0.8.3
-pip install -q tensorboardX>=1.4
-pip install -q runx==0.0.6
+pip install -q pyyaml
+pip install -q coolname
+pip install -q tabulate
+pip install -q tensorboardX
+pip install -q runx
