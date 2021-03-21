dfc99562099d19a434b0317213460e94c08d5f88
bitotest <bitotest@bito.ltd>
2021-03-22 00:04:29 +0800

    change envs pip to pip3

--- envs/env_for_google_colab.sh
+++ envs/env_for_google_colab.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 git clone https://github.com/NVIDIA/apex.git apex && cd apex && python3 setup.py install
 
-pip install -q torch_snippets
-pip install -q pyyaml
-pip install -q coolname
-pip install -q tabulate
-pip install -q tensorboardX
-pip install -q runx
+pip3 install -q torch_snippets
+pip3 install -q pyyaml>=5.1.1
+pip3 install -q coolname>=1.1.0
+pip3 install -q tabulate>=0.8.3
+pip3 install -q tensorboardX>=1.4
+pip3 install -q runx==0.0.6
